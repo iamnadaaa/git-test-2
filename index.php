bc1393ba5ac3269b49ebfecde0e8b9e4b5fff0b2
iamnadaaa <nadasalsabilah19@gmail.com>
2022-02-21 13:10:22 +0700

    Menambhakna paraghraph

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <h1> hello world <h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsum accusamus similique. Laborum nihil blanditiis laboriosam laudantium illo delectus consequuntur ullam fuga possimus, est praesentium quas numquam ipsam voluptate magnam.
+</body>
+</html>
